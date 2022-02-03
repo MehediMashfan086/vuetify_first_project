bcba40d078ca8e5306daa421e02c920f28ed9344
MehediMashfan086 <mehedihasan62086@gmail.com>
2022-02-03 09:56:08 +0600

    Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,12 @@
     <v-col lg="12">
         <v-row justify="center">
             <v-col lg="4" class="text-center">
-                <v-toolbar-totle>PhotoGallery</v-toolbar-totle>
+                <v-toolbar-title>PhotoGallery</v-toolbar-title>
                 <v-card-text>
                     <v-btn 
                         v-for="icon in icons" 
                         :key="icon" 
-                        class="mx-4 white--text" 
+                        class="mx-4 black--text" 
                         icon
                     >
                         <v-icon size="24px">
@@ -26,19 +26,15 @@
                 </v-form>
             </v-col>
         </v-row>
+            <div mt-10>
+                <v-divider></v-divider>
+                <v-subheader>
+                    © Copyright PhotoGallery. All Rights Reserved {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+                    <v-spacer />
+                    <strong>Designed by Mehedi Hasan</strong>
+                </v-subheader>
+            </div>
     </v-col>
-    <v-card flat tile class="indigo lighten-1 white--text text-center">
-
-        <v-card-text class="white--text pt-0">
-            Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-text class="white--text">
-            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-        </v-card-text>
-    </v-card>
 </v-footer>
 </template>
 
